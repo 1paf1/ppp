de31ff2149e36a59214cd9bb041e28587e277c8d
Haisenberg <1paf1@ukr.net>
2023-12-30 14:50:50 +0200

    first task

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # hello
+
+
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
+
+operation = input("Виберіть операцію (max, min, avg): ")
+
+if operation == "max":
+    result = max(num1, num2, num3)
+    print(f"Максимум: {result}")
+elif operation == "min":
+    result = min(num1, num2, num3)
+    print(f"Мінімум: {result}")
+elif operation == "avg":
+    result = (num1 + num2 + num3) / 3
+    print(f"Середнє арифметичне: {result}")
+else:
+    print("Невірна операція. Будь ласка, виберіть 'max', 'min' або 'avg'.")
