104f26c99a755498a8f8101b81497f501a3349e7
Haisenberg <1paf1@ukr.net>
2023-12-30 14:51:20 +0200

    second task

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # hello
+
+
+
+meters = float(input("Введіть кількість метрів: "))
+
+
+operation = input("Виберіть операцію (mile, inch, yard): ")
+
+
+if operation == "mile":
+    result = meters * 0.000621371
+    print(f"{meters} метрів дорівнює {result} миль")
+elif operation == "inch":
+    result = meters * 39.3701
+    print(f"{meters} метрів дорівнює {result} дюймів")
+elif operation == "yard":
+    result = meters * 1.09361
+    print(f"{meters} метрів дорівнює {result} ярдів")
+else:
+    print("Невірна операція. Будь ласка, виберіть 'mile', 'inch' або 'yard'.")
